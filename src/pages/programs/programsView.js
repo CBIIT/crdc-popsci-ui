ce5a56ab2ed1859b89b353bd685940991e8417a5
Nahomtes <44936471+Nahomtes@users.noreply.github.com>
2025-07-08 10:30:49 -0400

    add maxContentWidth to other maxWidth classes

--- src/pages/programs/programsView.js
+++ src/pages/programs/programsView.js
@@ -79,7 +79,7 @@ const styles = (theme) => ({
   },
   container: {
     margin: 'auto',
-    maxWidth: '1440px',
+    maxWidth: `${theme?.custom?.maxContentWidth || '1440px'}`,
     paddingLeft: '36px',
     paddingRight: '36px',
   },
@@ -116,7 +116,7 @@ const styles = (theme) => ({
   },
 
   headerTitle: {
-    maxWidth: '1440px',
+    maxWidth: `${theme?.custom?.maxContentWidth || '1440px'}`,
     margin: 'auto',
     float: 'left',
     marginLeft: '90px',
