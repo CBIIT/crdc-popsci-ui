5dd5007a1f41764a1eb6f7b7268b38917cf2403f
Nahomtes <44936471+Nahomtes@users.noreply.github.com>
2025-08-11 03:09:04 -0400

    Refactor IconCell rendering logic in CustomCellView
    Simplified the renderIconCell helper by clarifying conditions for 'accessControl' and 'fileDelivery' types. Removed unused props and redundant tooltipPlacement property.

--- src/pages/studyDetail/views/studyFiles/tableConfig/Column.js
+++ src/pages/studyDetail/views/studyFiles/tableConfig/Column.js
@@ -15,26 +15,24 @@ export const CustomCellView = (props) => {
 
   // Helper to render IconCell for access control/file delivery
   const renderIconCell = (type) => {
-    if (props[dataField] === "Open Access") {
+    if (type === 'accessControl' && props[dataField] === "Open Access") {
       return (
         <IconCell
           signedUrl={props[dataField]}
           toolTipText={customCellProps?.openAccessTooltip}
           iconSrc={customCellProps?.openAccessIcon}
           showToolTip={true}
-          tooltipPlacement={'top'}
-          {...(type === 'fileDelivery' && { onAction: (url) => console.log(`Open Access Downloading ${url}`) })}
         />
       );
     }
-    if (props[dataField] === "Controlled Access") {
+    // All Files can only be accessed through Seven Bridges Cancer Genomics Cloud 
+    if (type === 'fileDelivery' || props[dataField] === "Controlled Access") {
       return (
         <IconCell
           signedUrl={props[dataField]}
           toolTipText={customCellProps?.controlledAccessTooltip}
           iconSrc={customCellProps?.controlledAccessIcon}
           showToolTip={true}
-          tooltipPlacement={'top'}
         />
       );
     }
