d5588290b89cc7e072e8e1427930be89b874d19f
Nahomtes <44936471+Nahomtes@users.noreply.github.com>
2025-08-11 03:24:41 -0400

    Fix tooltip icon position for adding Files to the cart
    Deleted the 'marginTop: 6px' style from the '& img' selector in the customTheme object to adjust image alignment.

--- src/pages/studyDetail/views/studyFiles/wrapperConfig/Theme.js
+++ src/pages/studyDetail/views/studyFiles/wrapperConfig/Theme.js
@@ -20,7 +20,6 @@ export const customTheme = {
       },
       '& img': {
         verticalAlign: 'top',
-        marginTop: '6px',
         // '&.addAllTooltip': {},
         // '&.addSelectedFileTooltip': {},
       },
