7098096e62f75b2d48aaec19ab615d6d097d0dc4
nathanhuangnih <nathaniel.huang@nih.gov>
2025-09-09 08:56:56 -0400

    remove change to tooltip max width

--- src/components/BarChartV2/bar-chart-v2.js
+++ src/components/BarChartV2/bar-chart-v2.js
@@ -60,7 +60,7 @@ const CustomTooltip = ({ active, payload, label }) => {
     padding: '10px',
     borderRadius: '5px',
     overflowWrap: 'break-word',
-    maxWidth: '160px',
+    maxWidth: '170px',
   };
 
   if (active && payload && payload.length) {
