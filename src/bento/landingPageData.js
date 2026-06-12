0e1ea36545f8d6426e51a1dedbb5a87c75a67e72
Nahomtes <44936471+Nahomtes@users.noreply.github.com>
2026-06-12 13:26:35 -0400

    Remove commented-out details in landingPageData

--- src/bento/landingPageData.js
+++ src/bento/landingPageData.js
@@ -25,7 +25,7 @@ export const landingPageData = {
     img: 'https://raw.githubusercontent.com/CBIIT/datacommons-assets/main/popsci/images/icons/png/homeImageforAboutCard.png',
     titleText: 'About the Population Science Data Commons (PSDC)',
     descriptionText:
-      'Population science research aims to understand the causes and distribution of cancer in populations, monitor and explain cancer trends across different groups defined by geography or demographics, and support the development and implementation of broad-based interventions. The PSDC manages, houses, and shares data from various NCI-funded research programs and awards.', //It currently focuses on comprehensive study data with searchable summary data, allowing users to identify potential studies of interest using parameters including study name, study design, enrollment period, number of participants, cancer types, demographics such as age at enrollment, race, ethnicity and sex, and whether or not particular categories of data elements were collected as part of the study.',
+      'Population science research aims to understand the causes and distribution of cancer in populations, monitor and explain cancer trends across different groups defined by geography or demographics, and support the development and implementation of broad-based interventions. The PSDC manages, houses, and shares data from various NCI-funded research programs and awards.',
     callToActionText: 'Read More',
     callToActionLink: '/about', // This links to the 'About' static page.
   },
